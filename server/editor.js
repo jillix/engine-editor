9c25897ccdaaf8de8a59d2db07319d11ca1582db
Adrian Ottiker <adrian@ottiker.com>
2014-03-07 04:26:18 +0100

    emit "ready" event after init

--- server/editor.js
+++ server/editor.js
@@ -2,7 +2,14 @@ var M = process.mono;
 var View = require(M.config.paths.MODULE_ROOT + 'github/jillix/view/v0.0.1/server/view');
 
 function init (config) {
-    View(this);
+    var self = this;
+    
+    // plug View
+    View(self, function (err) {
+        
+        // instance is ready
+        self.emit('ready', err);
+    });
 }
 
 module.exports = init;
