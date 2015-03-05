09586cba094a571fb2e1071c55b97b9136b2dbc0
Ionică Bizău <bizauionica@yahoo.com>
2015-03-05 16:13:11 +0200

    JSDoc coments, init editor and set options

--- client/index.js
+++ client/index.js
@@ -1,18 +1,58 @@
-exports.set = function () {
+// Dependencies
+var Ace = require("./libs/ace-builds/src-noconflict/ace");
 
+/**
+ * set
+ * Sets the new editor value.
+ *
+ * @name set
+ * @function
+ * @param {Event} ev The event object
+ * @param {String} data The new value.
+ * @return {undefined}
+ */
+exports.set = function (ev, data) {
+    this.editor.setValue(data);
 };
 
-exports.get = function () {
-
+/**
+ * get
+ * Gets the editor value.
+ *
+ * @name get
+ * @function
+ * @param {Event} ev The event object
+ * @param {An object containing the callback function.} data
+ * @return {undefined}
+ */
+exports.get = function (ev, data) {
+    var value = this.editor.getValue();
+    if (typeof data === "function") {
+        data = { callback: data };
+    }
+    typeof data.callback && data.callback.call(this, value);
+    return value;
 };
 
+/**
+ * init
+ * The init function.
+ *
+ * @name init
+ * @function
+ * @return {undefined}
+ */
 exports.init = function () {
     var self = this;
-    //setup editor
-    self.editor.setTheme("ace/theme/textmate");
 
-    // set font size
-    self.editor.setFontSize(13);
+    self.edEl.style.width = "100%";
+    self.edEl.style.height = "100%";
+
+    self.edEl = document.querySelector(self._config.editor);
+    self.editor = Ace.edit();
+    self.editor.setTheme("ace/theme/" + self._config.theme);
+    self.editor.setFontSize(self._config.font_size || 13);
+    self.editor.getSession().setMode("ace/mode/" + self._config.mode);
 
     // add ctrl-s command
     // self.editor.commands.addCommand({
@@ -26,10 +66,4 @@ exports.init = function () {
     //         saveDocument.call(self);
     //     }
     // });
-
-    // save automatically after 1s, if doc has changed
-    self.editor.getSession().on("change", function() {
-        self.changed = 1;
-        self.border.css('border-color', colors.change);
-    });
 };
